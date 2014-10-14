7fa767f213766d1ab841c6d988f731e0bc2d4742
Deborah Chan <deborahlchan@gmail.com>
2014-10-14 12:51:13 -0400

    Finished basic User model following https://www.railstutorial.org/book/modeling_users and added relationships between models

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,3 +1,5 @@
 class Passage < ActiveRecord::Base
+	belongs_to :prompt
+	belongs_to :user
 	validates :text, presence: true
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,3 +1,5 @@
 class Prompt < ActiveRecord::Base
+	has_many :passages
+	belongs_to :user
 	validates :text, presence: true
 end
