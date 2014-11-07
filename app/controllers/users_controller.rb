7aab6314981844b0f6f40cb62c7deb427433603a
Deborah Chan <deborahlchan@gmail.com>
2014-11-07 14:49:06 -0500

    Cleaned up Sign in and Sign up views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :gender)
     end
 
     # Confirms a logged-in user.
